04f207ef7452d720b9ef60002ec294c88b573c6e
Anikaaasingh <96921017+Anikaaasingh@users.noreply.github.com>
2023-01-17 10:33:55 +0530

    Update PageLoader.vue

--- PageLoader.vue
+++ PageLoader.vue
@@ -1,43 +1,39 @@
 <template>
-    <div class="page-loader" v-if="!isloaded">
-     
-    </div> 
-  </template>
   
-  <script>
-    export default{
-      data: () => {
-        return {
-          isloaded: false
-        }
-      },
-      mounted() {
-        document.onreadystatechange = () => {
-          if (document.readyState === "complete") { 
-            this.isloaded = true;
-          } 
-        }
-      },
+  <div v-if="loading" class="loading-page">
+    <div class="loading"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start() {
+      this.loading = true
+    },
+    finish() {
+      this.loading = false
     }
-  </script>
-  
-  <style lang="css" scoped>
-   
-  
-    .page-loader {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100vw;
-      height: 100vh;
-      background-image:url('Technex_Loader_MOV_AdobeExpress.gif'); background-repeat:repeat;
-      z-index: 999;
-    }
-  
-    
-  
-    
-  </style>
+  }
+}
+</script>
+
+<style scoped>
+
+.loading-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('./../static/Loader.gif');
+  background-repeat: no-repeat;
+  z-index: 999;
+}
+</style>
